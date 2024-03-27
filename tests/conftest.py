859cc60b4bd4209d2c9b4021584e9fb877a20a99
Sagi Hirshfeld <shirshfe@redhat.com>
2024-03-27 17:14:52 +0200

    Make fixes and adjustments to support other potential config roots

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,11 +17,12 @@ from noobaa_sa.s3_client import S3Client
 from utility.utils import (
     get_env_config_root_full_path,
     get_current_test_name,
+    get_noobaa_sa_host_home_path,
 )
 from utility.nsfs_server_utils import (
     restart_nsfs_service,
-    create_tls_key_and_cert,
-    set_nsfs_service_certs_dir,
+    check_nsfs_tls_cert_setup,
+    setup_nsfs_tls_cert,
 )
 
 
@@ -47,73 +48,56 @@ def bucket_manager(request):
 
 
 @pytest.fixture(scope="session")
-def setup_nsfs_server_config_root():
+def set_nsfs_server_config_root(request):
     """
-    Make sure the NSFS server is configured to use a custom config root directory
-    if one was provided in the CI configuration.
+    Returns a function that allows
+    configuring the NSFS service to use a custom config root dir
 
     """
     conn = SSHConnectionManager().connection
-    env_config_root_path = get_env_config_root_full_path()
 
-    # Check if the provided config root path exists on the remote machine
-    retcode, _, _ = conn.exec_cmd(f"sudo ls {env_config_root_path}")
-    if retcode != 0:
-        raise FileNotFoundError(
-            f"Config root path '{env_config_root_path}' not found on the remote machine"
-        )
+    # Ensure to reset to the default config root on teardown
+    def _clear_config_dir_redirect():
+        conn.exec_cmd("sudo rm -f /etc/noobaa.conf.d/config_dir_redirect")
+        restart_nsfs_service()
 
-    # If the config root path is different from the default, update the NSFS service
-    if env_config_root_path != constants.DEFAULT_CONFIG_ROOT_PATH:
-        # Loosen the permissions on the config root dir for necessary file commands
-        _, stdout, _ = conn.exec_cmd(f"stat -c '%a' {env_config_root_path}")
-        if stdout != "777":
-            conn.exec_cmd(f"sudo chmod 777 {env_config_root_path}")
+    def _redirect_nsfs_service_to_use_custom_config_root(config_root):
+        """
+        Configure the NSFS service to use a custom config root dir
 
-        # The presence of the system.json file in the dir
-        # indicates that the NSFS service uses it as the config root
-        _, stdout, _ = conn.exec_cmd(f"sudo ls {env_config_root_path}")
-        if "system.json" not in stdout:
-            conn.exec_cmd(
-                f"echo '{env_config_root_path}' | sudo tee /etc/noobaa.conf.d/config_dir_redirect"
+        Args:
+            config_root (str): The full path to the configuration root directory on the remote machine
+
+        """
+
+        # Skip if the provider config is the default just clear the config root redirect
+        if config_root == constants.DEFAULT_CONFIG_ROOT_PATH:
+            _clear_config_dir_redirect()
+            return
+
+        # Skip if the provided config root path is already set
+        retcode, stdout, _ = conn.exec_cmd("cat /etc/noobaa.conf.d/config_dir_redirect")
+        if retcode == 0 and stdout.strip() == config_root:
+            return
+
+        # Make sure the provided config root path exists on the remote machine
+        retcode, _, _ = conn.exec_cmd(f"sudo mkdir -p {config_root}")
+        if retcode != 0:
+            raise FileNotFoundError(
+                "Failed to create the provided config root path on the remote machine"
             )
-            restart_nsfs_service()
 
-
-@pytest.fixture(scope="session")
-def setup_nsfs_server_tls_cert(setup_nsfs_server_config_root):
-    """
-    Configure the NSFS server TLS certification and download the certificate
-    in a local file.
-
-    Args:
-        config_root (str): The path to the configuration root directory.
-
-    """
-    conn = SSHConnectionManager().connection
-    config_root_path = get_env_config_root_full_path()
-    remote_credentials_dir = f"{config_root_path}/certificates"
-
-    # Create the TLS credentials and configure the NSFS service to use them
-    conn.exec_cmd(f"sudo mkdir -p {remote_credentials_dir}")
-    remote_tls_crt_path = create_tls_key_and_cert(remote_credentials_dir)
-    set_nsfs_service_certs_dir(remote_credentials_dir, config_root_path)
-    restart_nsfs_service()
-
-    # Download the certificate to a local file
-    with tempfile.NamedTemporaryFile(
-        prefix="tls_", suffix=".crt", delete=False
-    ) as local_tls_crt_file:
-        conn.download_file(
-            remotepath=remote_tls_crt_path,
-            localpath=local_tls_crt_file.name,
+        conn.exec_cmd(
+            f"echo '{config_root}' | sudo tee /etc/noobaa.conf.d/config_dir_redirect"
         )
+        restart_nsfs_service()
 
-    S3Client.static_tls_crt_path = local_tls_crt_file.name
+    request.addfinalizer(_clear_config_dir_redirect)
+    return _redirect_nsfs_service_to_use_custom_config_root
 
 
 @pytest.fixture
-def s3_client_factory(setup_nsfs_server_tls_cert, account_manager):
+def s3_client_factory(set_nsfs_server_config_root, account_manager):
     """
     Factory to create S3Client instances with given credentials.
 
@@ -130,6 +114,7 @@ def s3_client_factory(setup_nsfs_server_tls_cert, account_manager):
         endpoint_port=constants.DEFAULT_NSFS_PORT,
         access_and_secret_keys_tuple=None,
         verify_tls=True,
+        config_root=None,
     ):
         """
         Create an S3Client instance using the given credentials.
@@ -143,6 +128,20 @@ def s3_client_factory(setup_nsfs_server_tls_cert, account_manager):
             S3Client: An S3Client instance.
 
         """
+        if not config_root:
+            config_root = get_env_config_root_full_path()
+
+        # The following setup requires the full path of the config root
+        if config_root.startswith("~/"):
+            config_root = (
+                f'{get_noobaa_sa_host_home_path()}/{config_root.split("~/")[1]}'
+            )
+
+        set_nsfs_server_config_root(config_root)
+
+        if not check_nsfs_tls_cert_setup(config_root):
+            setup_nsfs_tls_cert(config_root)
+
         # Set the AWS access and secret keys
         access_key, secret_key = None, None
         if access_and_secret_keys_tuple is None:
