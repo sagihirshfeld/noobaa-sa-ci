20b1b33e164feeb5731f71bada4d4e9f12f6bf69
Sagi Hirshfeld <shirshfe@redhat.com>
2024-07-29 18:10:13 +0300

    Also test anonymous account management using usernames

--- tests/test_anonymous_access.py
+++ tests/test_anonymous_access.py
@@ -9,6 +9,7 @@ import botocore.handlers
 
 from framework.bucket_policies.bucket_policy import BucketPolicyBuilder
 from framework.customizations.marks import tier1
+from framework.ssh_connection_manager import SSHConnectionManager
 from noobaa_sa.exceptions import AccountCreationFailed
 from noobaa_sa.s3_client import S3Client
 
@@ -21,56 +22,91 @@ class TestAnonymousAccess:
     """
 
     @tier1
-    def test_anonymous_account_management(self, account_manager):
+    def test_anonymous_account_management(self, account_manager, linux_user_factory):
         """
         Test management of the anonymous account via the NooBaa CLI
-        1. Create an anonymous account
-        2. Check that its uid and gid match the ones we provided
-        3. Try creating a second anonymous account and check that it fails
-        3. Update the anonymous account with new uid and gid
-        4. Check that the uid and gid were updated
-        5. Delete the anonymous account
-        6. Check that the anonymous account was deleted
-
+        1. Create new Linux users on the NooBaa server
+        2. Create an anonymous account by providing a uid and gid
+        3. Check that its uid and gid match the ones we provided
+        4. Try creating a second anonymous account and check that it fails
+        5. Update the anonymous account with new uid and gid
+        6. Check that the uid and gid were updated
+        7. Update the anonymous account with a new user name
+        8. Check that the user name was updated
+        9. Delete the anonymous account
+        10. Check that the anonymous account was deleted
+        11. Create an anonymous account by providing a user name
+        12. Check that the created account has the provided user name
         """
-        # 1. Create an anonymous account
-        uid, gid = random.randint(1000, 2000), random.randint(1000, 2000)
-        account_manager.anonymous.create(uid=uid, gid=gid)
 
-        # 2. Check that its uid and gid match the ones we provided
-        anon_acc_nsfs_config = account_manager.anonymous.status()["nsfs_account_config"]
-        assert (
-            anon_acc_nsfs_config["uid"] == uid and anon_acc_nsfs_config["gid"] == gid
-        ), f"Expected uid: {uid}, gid: {gid}, got {anon_acc_nsfs_config}"
+        def _verify_anonymous_account(uid=None, gid=None, username=None):
+            """
+            Verify that the anonymous account has the expected uid, gid, or user name
+            """
+            anon_acc_nsfs_config = account_manager.anonymous.status()[
+                "nsfs_account_config"
+            ]
+            if username:
+                assert (
+                    anon_acc_nsfs_config["distinguished_name"] == username
+                ), f"Expected user: {username}, got {anon_acc_nsfs_config}"
+            elif uid and gid:
+                assert (
+                    anon_acc_nsfs_config["uid"] == uid
+                    and anon_acc_nsfs_config["gid"] == gid
+                ), f"Expected uid: {uid}, gid: {gid}, got {anon_acc_nsfs_config}"
+            else:
+                raise ValueError(
+                    "Either a valid uid and gid pair or a valid user name must be provided"
+                )
 
-        # 3. Try creating a second anonymous account and check that it fails
+        # 1. Create new Linux users on the NooBaa server
+        uid_a, gid_a, username_a = linux_user_factory()
+        uid_b, gid_b, username_b = linux_user_factory()
+
+        # 2. Create an anonymous account by providing a uid and gid
+        account_manager.anonymous.create(uid=uid_a, gid=gid_a)
+
+        # 3. Check that its uid and gid match the ones we provided
+        _verify_anonymous_account(uid=uid_a, gid=gid_a)
+
+        # 4. Try creating a second anonymous account and check that it fails
         try:
-            account_manager.anonymous.create(uid=uid, gid=gid)
+            account_manager.anonymous.create(uid=uid_b, gid=gid_b)
         except AccountCreationFailed as e:
-            if "AccountNameAlreadyExists" not in str(e):
+            if "AccountNameAlreadyExists" in str(e):
+                log.info("Creating a second anonymous account failed as expected")
+            else:
                 log.error(
                     "Creating a second anonymous account failed with unexpected error"
                 )
                 raise e
-            else:
-                log.info("Creating a second anonymous account failed as expected")
 
-        # 4. Update the anonymous account with new uid and gid
-        log.info(account_manager.anonymous.update(uid=1234, gid=5678))
+        # 5. Update the anonymous account with new uid and gid
+        log.info(account_manager.anonymous.update(uid=uid_b, gid=gid_b))
 
-        # 5. Check that the uid and gid were updated
-        anon_acc_nsfs_config = account_manager.anonymous.status()["nsfs_account_config"]
-        assert (
-            anon_acc_nsfs_config["uid"] == 1234 and anon_acc_nsfs_config["gid"] == 5678
-        ), f"Expected uid: 1234, gid: 5678, got {anon_acc_nsfs_config}"
+        # 6. Check that the uid and gid were updated
+        _verify_anonymous_account(uid=uid_b, gid=gid_b)
 
-        # 6. Delete the anonymous account
+        # 7. Update the anonymous account with a new user name
+        log.info(account_manager.anonymous.update(user=username_b))
+
+        # 8. Check that the user name was updated
+        _verify_anonymous_account(username=username_b)
+
+        # 9. Delete the anonymous account
         account_manager.anonymous.delete()
 
-        # 7. Check that the anonymous account was deleted
+        # 10. Check that the anonymous account was deleted
         assert (
             not account_manager.anonymous.status()
         ), "Anonymous account was not deleted"
+
+        # 11. Create an anonymous account by providing a user name
+        account_manager.anonymous.create(user=username_a)
+
+        # 12. Check that the created account has the provided user name
+        _verify_anonymous_account(username=username_a)
 
     @tier1
     def test_anonymous_access(self, account_manager, s3_client_factory):
